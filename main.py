9c63ba73a157736903e39784f50a54c1eb7e4a33
Prakhar singh <prakharsingh737@gmail.com>
2025-09-17 16:35:03 +0530

    updating bucket name

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import boto3
+from datetime import datetime
 
 def lambda_handler(event, context):
     # Initialize S3 client
     s3 = boto3.client('s3')
     
     # Specify your bucket and file details
-    bucket_name = "your-bucket-name"
-    file_key = "your-folder/your-file.txt"  # path inside bucket
+    bucket_name = "prakhar-02071998-bucket"
+    file_key = "prakhar_test.txt"  # path inside bucket
     
     try:
         # Read the existing file content
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
         content = response['Body'].read().decode('utf-8')
         
+        # Current date and time
+        now = datetime.now()
+        date_str = now.strftime("%Y-%m-%d")
+        time_str = now.strftime("%H:%M:%S")
+        
         # Add new text
-        new_text = "\nThis is the new text added from Lambda!"
+        new_text = f"\nDate: {date_str} and Time: {time_str} and Hello World"
         updated_content = content + new_text
         
         # Write the updated content back to S3
